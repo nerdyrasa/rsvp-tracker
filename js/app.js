8de2b48646c959baf159eb2249ef01552c34ee5c
nerdyrasa <nerdyrasa@users.noreply.github.com>
2017-01-13 12:20:50 -0600

    Add functionality to remove name.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,7 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const text = input.value;
-  input.value = '';
+function createLi(text) {
   const li = document.createElement('li');
   li.textContent = text;
   const label = document.createElement('label');
@@ -14,8 +11,21 @@ form.addEventListener('submit', (e) => {
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
+  const button = document.createElement('button');
+  button.textContent = 'remove';
+  li.appendChild(button);
+  return li;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  input.value = '';
+  const li = createLi(text);
   ul.appendChild(li);
 });
+
+
 
 ul.addEventListener('change', (e) => {
   const checkbox = event.target;
@@ -23,5 +33,14 @@ ul.addEventListener('change', (e) => {
   const listItem = checkbox.parentNode.parentNode;
 
   listItem.className = checked ? 'responded': '';
+
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+  }
 
 });

2f4d1393714d31bad3227a7cae247affd8a88dad
nerdyrasa <nerdyrasa@users.noreply.github.com>
2017-01-14 09:38:09 -0600

    Create an text input when user clicks on edit button.

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ const ul = document.getElementById('invitedList');
 
 function createLi(text) {
   const li = document.createElement('li');
-  li.textContent = text;
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   label.appendChild(checkbox);
   li.appendChild(label);
-  const button = document.createElement('button');
-  button.textContent = 'remove';
-  li.appendChild(button);
+  const editButton = document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
   return li;
 }
 
@@ -23,8 +28,8 @@ form.addEventListener('submit', (e) => {
   input.value = '';
   const li = createLi(text);
   ul.appendChild(li);
-});
-
+})
+;
 
 
 ul.addEventListener('change', (e) => {
@@ -32,15 +37,30 @@ ul.addEventListener('change', (e) => {
   const checked = checkbox.checked;
   const listItem = checkbox.parentNode.parentNode;
 
-  listItem.className = checked ? 'responded': '';
+  listItem.className = checked ? 'responded' : '';
 
-});
+})
+;
 
 ul.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    const li = e.target.parentNode;
+  if (e.target.tagName === 'BUTTON')
+  {
+    const button = e.target;
+    const li = button.parentNode;
     const ul = li.parentNode;
-    ul.removeChild(li);
+
+    if (button.textContent === 'remove') {
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') {
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
+    }
   }
 
-});
+})
+;
